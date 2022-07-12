18e6bb81bbd8050d4ea5a3150097f8f5874c5905
aTfure <OCHIENG.FREDONYANGO@GMAIL.COM>
2022-07-12 17:05:44 +0300

    Add files via upload

---
+++ printf_flag_zero.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - handles the field width 
+* for non-custom conversion specifiers.
+*/
+
+int main()
+{
+	char grade = 'A';
+	char name[] = "Sbusiso";
+	char course[] = "ALX Software Engineering";
+	int age = 32;
+	double average = 145.2;
+	
+	printf(%s studies %s\n, name, course);
+	printf(%d year old %s studies %c grade %s\n, age, name, grade, course);
+	printf("The average score for %s is %.1lf.\n", name, average);
+	
+	printf("%02.1lf\n, average);
+	printf("%01d\n, age);
+	return 0;
+}
